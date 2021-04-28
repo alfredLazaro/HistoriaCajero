d92a62cb26e60bb07be7593eb8c51bc754b5afe3
javierFili <75696158+javierFili@users.noreply.github.com>
2021-04-27 20:32:10 -0400

    Update HistoriaCajero.java

--- HistoriaCajero/src/historiacajero/HistoriaCajero.java
+++ HistoriaCajero/src/historiacajero/HistoriaCajero.java
@@ -15,7 +15,8 @@ public class HistoriaCajero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here\
+        int nose;
     }
     
 }
